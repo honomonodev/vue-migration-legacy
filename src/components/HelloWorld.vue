ce07ea71b0309528da4e36f1d4585a906a4a5188
GitRockie <150247498+honomonodev@users.noreply.github.com>
2024-12-03 14:37:33 +0100

    Deploy to Heroku with Tailwind CSS and Google Fonts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="text-center">
-      <h1 class="text-4xl font-bold text-blue-500">Hello World</h1>
-      <p class="mt-4 text-lg text-gray-700">
-        Tailwind CSS is working!
+      <h1 class="text-4xl font-bold text-blue-500 font-maven">Hello World</h1>
+      <p class="mt-4 text-lg text-gray-700 font-maven">
+        Tailwind CSS is working with Maven Pro font!
       </p>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+// tailwind.config.js
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: "media", // or remove this line
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        maven: ['"Maven Pro"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };

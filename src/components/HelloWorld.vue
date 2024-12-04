771861060e59b771c747c893f92a6b468e44f762
GitRockie <150247498+honomonodev@users.noreply.github.com>
2024-12-04 15:34:01 +0100

    Tailwind install
    Init/Config/create/Import

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="example-component p-4 bg-gray-100 rounded-lg">
+    <h1 class="text-2xl font-bold text-gray-800">Example Component</h1>
+    <p class="text-gray-600">This is a simple component styled with Tailwind CSS.</p>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const msg = ref('Hello, Vue 3!');
+const message = ref('Hello, Vue 3!');
 </script>
 
 <style scoped>
-.hello {
+.example-component {
   padding: 20px;
 }
 </style>
